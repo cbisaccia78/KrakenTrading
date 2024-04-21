d2076e9470cbe26132d37d88b7ca5f2cfb4b7756
cbisaccia78 <FEFQ9F1qaz0>
2024-04-21 15:56:32 -0400

    moved code to utilities

--- database.py
+++ database.py
@@ -1,18 +1,8 @@
 import json
 
-from utilities import flatten_lists
-
 def get_ticker(filename):
     ticker = []
     with open(filename, 'r') as fp:
         ticker = fp.read().split('\n')
         ticker = [json.loads(pair) for pair in ticker]
     return ticker
-
-def vectorize_ticker(ticker=[]):
-    last_known_pair_info = {}
-
-    for pair in ticker:
-        time_received = list(pair.keys())[0]
-        data = pair[time_received]['data'].values()
-        flattened_data = flatten_lists(data)
