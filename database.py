3a6526fe6ece24a41a2e13557d447370e85feeeb
cole <cbisaccia78@gmail.com>
2024-04-21 22:41:18 -0400

    bug fix

--- database.py
+++ database.py
@@ -4,11 +4,12 @@ def get_ticker_stream(filename):
     ticker_stream = []
     with open(filename, 'r') as fp:
         raw_ticker_stream = fp.read().split('\n')
+        raw_ticker_stream.pop(-1)
         for pair in raw_ticker_stream:
             try:
                 p = json.loads(pair)
                 ticker_stream.append(p)
             except Exception as e:
-                print(pair)
+                print(e)
                 return []
     return ticker_stream
