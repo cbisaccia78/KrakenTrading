67ba11a7278650a53a95c22a2251b20fda2ab610
cbisaccia78 <FEFQ9F1qaz0>
2024-04-28 11:30:11 -0400

    starting work on live prediction

--- high_bid_model.py
+++ high_bid_model.py
@@ -2,6 +2,7 @@ import numpy as np
 import keras as keras
 import tensorflow as tf
 from sklearn.preprocessing import StandardScaler
+from keras.callbacks import EarlyStopping
 
 from database import get_ticker_stream
 from utilities import vectorize_ticker_stream, timestamp_to_percent, vectorize_windows
@@ -18,80 +19,84 @@ FEATURE_MAP = {
     'open_price_today': 18, 'open_price_last_24': 19
 }
 
-raw_ticker_stream = get_ticker_stream('/Users/dancing_ghosts/Code/WebApps/KrakenTrading/ticker-1-test')
-ticker_stream, all_pairs_sorted = vectorize_ticker_stream(raw_ticker_stream) # 200000 is approximately 28 gb of memory, 100000 is approximately 17gb
+def create_model(ticker_stream_path, pair_name, window_len=5, stride=1):
 
-btc_index = all_pairs_sorted.index('XBT/USD')
-num_pairs = len(all_pairs_sorted)
-features_per_pair = 20
+    raw_ticker_stream = get_ticker_stream(ticker_stream_path)
+    ticker_stream, all_pairs_sorted = vectorize_ticker_stream(raw_ticker_stream) # 200000 is approximately 28 gb of memory, 100000 is approximately 17gb
 
-# create evenly spaced samples over time (not implemented yet)
+    btc_index = all_pairs_sorted.index(pair_name)
+    features_per_pair = 20
 
-X = np.array(ticker_stream, dtype=np.float32)
+    # create evenly spaced samples over time (not implemented yet)
 
-X[:, 0] = timestamp_to_percent(X[:, 0])
+    X = np.array(ticker_stream, dtype=np.float32)
 
-# train test validation split
-num_examples = X.shape[0]
+    X[:, 0] = timestamp_to_percent(X[:, 0])
 
-train_valid_split = int(0.8*num_examples)
+    # train test validation split
+    num_examples = X.shape[0]
 
-X_train_valid = X[0:train_valid_split]
+    train_valid_split = int(0.8*num_examples)
 
-train_split = int(0.8*train_valid_split)
-X_train = X_train_valid[0:train_split]
-X_valid = X_train_valid[train_split:]
+    X_train_valid = X[0:train_valid_split]
 
-X_test = X[train_valid_split:]
+    train_split = int(0.8*train_valid_split)
+    X_train = X_train_valid[0:train_split]
+    X_valid = X_train_valid[train_split:]
 
-# Standardize features
-scalar = StandardScaler()
-X_train_std = scalar.fit_transform(X_train)
-X_valid_std = scalar.transform(X_valid)
-X_test_std = scalar.transform(X_test)
+    X_test = X[train_valid_split:]
 
-window_len = 20
-stride = 1
+    # Standardize features
+    scalar = StandardScaler()
+    X_train_std = scalar.fit_transform(X_train)
+    X_valid_std = scalar.transform(X_valid)
+    X_test_std = scalar.transform(X_test)
 
-# create labels
-bid_index = FEATURE_MAP['best_bid']
-btc_bid_index = btc_index*features_per_pair + bid_index
+    # create labels
+    bid_index = FEATURE_MAP['best_bid']
+    btc_bid_index = btc_index*features_per_pair + bid_index
 
-y_train_std = X_train_std[window_len:, btc_bid_index]
-y_valid_std = X_valid_std[window_len:, btc_bid_index]
-y_test_std = X_test_std[window_len:, btc_bid_index]
+    y_train_std = X_train_std[window_len:, btc_bid_index]
+    y_valid_std = X_valid_std[window_len:, btc_bid_index]
+    y_test_std = X_test_std[window_len:, btc_bid_index]
 
-# create windows
-X_train_std = vectorize_windows(X_train_std, window_len, stride)
-X_valid_std = vectorize_windows(X_valid_std, window_len, stride)
-X_test_std = vectorize_windows(X_test_std, window_len, stride)
+    # create windows
+    X_train_std = vectorize_windows(X_train_std, window_len, stride)
+    X_valid_std = vectorize_windows(X_valid_std, window_len, stride)
+    X_test_std = vectorize_windows(X_test_std, window_len, stride)
 
-# remove final row (no label for it)
-X_train_std = X_train_std[:-1]
-X_valid_std = X_valid_std[:-1]
-X_test_std = X_test_std[:-1]
+    # remove final row (no label for it)
+    X_train_std = X_train_std[:-1]
+    X_valid_std = X_valid_std[:-1]
+    X_test_std = X_test_std[:-1]
 
-batch_size = 32
+    batch_size = 32
 
-# create datasets
-train_ds = tf.data.Dataset.from_tensor_slices((X_train_std, y_train_std)).batch(batch_size)
-valid_ds = tf.data.Dataset.from_tensor_slices((X_valid_std, y_valid_std)).batch(batch_size)
-test_ds = tf.data.Dataset.from_tensor_slices((X_test_std, y_test_std)).batch(batch_size)
+    # create datasets
+    train_ds = tf.data.Dataset.from_tensor_slices((X_train_std, y_train_std)).batch(batch_size)
+    valid_ds = tf.data.Dataset.from_tensor_slices((X_valid_std, y_valid_std)).batch(batch_size)
+    test_ds = tf.data.Dataset.from_tensor_slices((X_test_std, y_test_std)).batch(batch_size)
 
 
-# create model
-num_input_parameters = X_train_std.shape[1]
-model = keras.Sequential([
-    keras.layers.Dense(1024, activation='relu', input_shape=(None, num_input_parameters)),
-    keras.layers.Dropout(rate=0.5),
-    keras.layers.Dense(1024, activation='relu'),
-    keras.layers.Dense(1, activation='linear')
-])
+    # create model
+    num_input_parameters = X_train_std.shape[1]
+    model = keras.Sequential([
+        keras.layers.Dense(1024, activation='relu', input_shape=(None, num_input_parameters)),
+        keras.layers.Dropout(rate=0.5),
+        keras.layers.Dense(1024, activation='relu'),
+        keras.layers.Dense(1, activation='linear')
+    ])
 
-model.compile(optimizer='adam', loss='mse', metrics=['mse'])
+    model.compile(optimizer='adam', loss='mse', metrics=['mse'])
 
-# fit training data
-history = model.fit(train_ds, epochs=10, validation_data=valid_ds)
+    early_stopping_callback = EarlyStopping(monitor='val_mse', patience=20, restore_best_weights=True)
 
-# evaluate test data
-test_loss, test_mse = model.evaluate(test_ds)
+    # fit training data
+    history = model.fit(train_ds, epochs=250, validation_data=valid_ds, callbacks=[early_stopping_callback])
+
+    # evaluate test data
+    test_loss, test_mse = model.evaluate(test_ds)
+
+    print('test_mse : {}'.format(test_mse))
+
+    return model
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 import datetime
 
 from api import get_all_wsnames
+from high_bid_model import create_model
 
 PING = {
     "event": "ping",
@@ -12,7 +13,7 @@ PING = {
 PUBLIC_URL = "wss://ws.kraken.com/"
 PRIVATE_URL = "wss://ws-auth.kraken.com/"
 
-with open('./ticker-1', 'a') as ticker_fp:
+with open('./ticker-1-test', 'a') as ticker_fp:
 
     # Function to handle incoming messages
     def on_message(ws, message):
@@ -27,6 +28,7 @@ with open('./ticker-1', 'a') as ticker_fp:
     # Function to handle WebSocket open event
     def on_open(ws):
         print("WebSocket connection opened")
+        'XBT/USD'
         # Subscribe to the desired channel(s) after the connection is open
         # Send the subscription message to the WebSocket server
         subscription_message = {
@@ -42,11 +44,29 @@ with open('./ticker-1', 'a') as ticker_fp:
     def on_close(ws):
         print("WebSocket connection closed")
 
+    count = 0
+
+    NUM_EXAMPLES = 4459
+
     # Create a WebSocket connection
     ws = websocket.WebSocketApp("wss://ws.kraken.com/", on_message=on_message, on_open=on_open, on_close=on_close)
 
+    """
+    Need a separate thread of execution here which, upon reaching NUM_EXAMPLES messages, will create a model from these examples.
+    As the model is being created, more data will be streaming in. Call this extra data X_extra. 
+    After the model is created, the main thread should go to the most recent X_extra examples and start prediction. 
+
+    Question: Should there be "backfilling" of this unused X_extra data? Presumably this thread could online-train 
+    this extra data in between attempts at predicting the most recent data, considering that we are throttled by 
+    120 trades per minute. On first cut this data will just be lost. 
+
+    THe main thread should also be checking the mse score for incoming data, and if the score deteriorates past a certain threshold,
+    should tell the model thread to train a new model with more recent data. This implies that data that we run predictions on should
+    be stored for future model training. 
+    """
+
     # Start the WebSocket connection (runs in a separate thread)
-    ws.run_forever()
+    ws.run_forever() # this hangs until websocket is stopped
 
     # Close websocket gracefully
     ws.close()
