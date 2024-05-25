fb00680c9fdb2d030ab1001e24791fe777cd2938
cbisaccia78 <FEFQ9F1qaz0>
2024-05-25 17:09:10 -0400

    allow for short-stopping with keyboardinterrupt

--- trade.py
+++ trade.py
@@ -2,6 +2,7 @@ import websocket
 import json
 import datetime
 import threading
+import signal
 
 import numpy as np
 
@@ -22,6 +23,7 @@ window_length = 5
 
 model = None
 model_thread = None
+model_thread_stop = False
 
 retrain = False
 
@@ -33,11 +35,25 @@ errors = []
 examples_processed = 0
 examples_received = 0
 
-NUM_EXAMPLES = 4459
+NUM_EXAMPLES = 700 # 4459
 
 def model_thread_func():
+    """
+    Separate thread of execution which, upon reaching NUM_EXAMPLES messages, will create a model from these examples.
+    As the model is being created, more data will be streaming in. Call this extra data X_extra. 
+    After the model is created, the thread updates to the most recent X_extra examples and start prediction. 
+
+    Question: Should there be "backfilling" of this unused X_extra data? Presumably this thread could online-train 
+    this extra data in between attempts at predicting the most recent data, considering that we are throttled by 
+    120 trades per minute. On first cut this data will just be lost. 
+
+    The thread should also be checking the mse score for incoming data, and if the score deteriorates past a certain threshold,
+    should tell the model thread to train a new model with more recent data. This implies that data that we run predictions on should
+    be stored for future model training. 
+    """
 
     global model
+    global model_thread_stop
     global raw_ticker_stream
     global pair_cache
     global examples_processed
@@ -45,45 +61,48 @@ def model_thread_func():
     global retrain
     global errors
 
-    if model is None: # need to create model
-            if examples_received >= NUM_EXAMPLES: 
-                model, test_mse, standard_scalar, pair_cache = create_model(raw_ticker_stream, pair_name, window_len=window_length)
-                examples_processed = NUM_EXAMPLES
-    
-    elif retrain: # model is not performing well
-        examples_received = 0
-        examples_processed = 0
-        model = None
-
-    else: # we can trade
-
-        # update the pair cache for all examples that came in while model was trading / sleeping,
-        # UNLESS those examples will be apart of the window_length
-        last_uncached_index = examples_received - window_length
-        if last_uncached_index - examples_processed > 0:
-            update_pair_cache(raw_ticker_stream[examples_processed:last_uncached_index], pair_cache)
-
-        # grab the most recent window_length examples
-        example_window = raw_ticker_stream[last_uncached_index:]
-        # vectorize examples from the cache
-        example_window = vectorize_from_cache(pair_cache, example_window) # this call will also update pair cache with recent examples
+    while not model_thread_stop:
+        if model is None: # need to create model
+                if examples_received >= NUM_EXAMPLES:
+                    print('>')
+                    model, test_mse, standard_scalar, pair_cache = create_model(raw_ticker_stream, pair_name, window_len=window_length)
+                    examples_processed = NUM_EXAMPLES
         
-        #convert to numpy array
-        x = np.array(example_window, dtype=np.float32)
-        x[:, 0] = timestamp_to_percent(x[:, 0])
+        elif retrain: # model is not performing well
+            examples_received = 0
+            examples_processed = 0
+            model = None
 
-        # standardize using same mean/std from creation of model
-        x = standard_scalar.transform(x)
+        else: # we can trade
 
-        x = vectorize_window(x, window_length)
+            # update the pair cache for all examples that came in while model was trading / sleeping,
+            # UNLESS those examples will be apart of the window_length
+            _examples_received = examples_received # just in case this thread gets put to sleep and examples received changes, we don't want to get out of sync
+            last_uncached_index = _examples_received - window_length
+            if last_uncached_index - examples_processed > 0:
+                update_pair_cache(raw_ticker_stream[examples_processed:last_uncached_index], pair_cache)
 
-        # Reshape the example to add a batch dimension
-        x = np.reshape(x, (1,) + x.shape)
+            # grab the most recent window_length examples
+            example_window = raw_ticker_stream[last_uncached_index:]
+            # vectorize examples from the cache
+            example_window = vectorize_from_cache(pair_cache, example_window) # this call will also update pair cache with recent examples
+            
+            #convert to numpy array
+            x = np.array(example_window, dtype=np.float32)
+            x[:, 0] = timestamp_to_percent(x[:, 0])
 
-        prediction = model(x)
+            # standardize using same mean/std from creation of model
+            x = standard_scalar.transform(x)
 
-        print(prediction)
-    
+            x = vectorize_window(x, window_length)
+
+            # Reshape the example to add a batch dimension
+            x = np.reshape(x, (1,) + x.shape)
+
+            prediction = model(x)
+
+            examples_processed = _examples_received # use local value in case global one was updated while this thread was sleeping
+    print('done')
 # Function to handle incoming messages
 def on_message(ws, message):
     global examples_received
@@ -96,6 +115,7 @@ def on_message(ws, message):
     now = str(datetime.datetime.now())
     raw_ticker_stream.append({now: {'pair': pair, 'data': data}})
     examples_received = examples_received + 1
+    print(examples_received)
 
 # Function to handle WebSocket open event
 def on_open(ws):
@@ -114,23 +134,14 @@ def on_open(ws):
 
 # Function to handle WebSocket close event
 def on_close(ws):
+    global model_thread
+    global model_thread_stop
     print("WebSocket connection closed")
     # join threads
+    model_thread_stop = True
     model_thread.join()
 
-"""
-Need separate threads of execution here which, upon reaching NUM_EXAMPLES messages, will create a model from these examples.
-As the model is being created, more data will be streaming in. Call this extra data X_extra. 
-After the model is created, the main thread should go to the most recent X_extra examples and start prediction. 
-
-Question: Should there be "backfilling" of this unused X_extra data? Presumably this thread could online-train 
-this extra data in between attempts at predicting the most recent data, considering that we are throttled by 
-120 trades per minute. On first cut this data will just be lost. 
-
-THe trading thread should also be checking the mse score for incoming data, and if the score deteriorates past a certain threshold,
-should tell the model thread to train a new model with more recent data. This implies that data that we run predictions on should
-be stored for future model training. 
-"""
+signal.signal(signal.SIGINT, )
 
 model_thread = threading.Thread(target=model_thread_func)
 
